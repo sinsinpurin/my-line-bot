2f2e48d408f730bbb633837351f72a7ce9c27848
sinsinpurin <tusanch6@gmail.com>
2021-07-30 03:37:40 +0900

    fix

--- src/app.js
+++ src/app.js
@@ -17,36 +17,20 @@ app.listen(port, () => {
     console.log(`listening on ${port}`);
 });
 
-app.post('/callback', (req, res) => {
-    res.sendStatus(200);
-    res.send('Hello world')
-})
-
 app.post('/callback', line.middleware(config), (req, res) => {
-    res.sendStatus(200);
-
-    // すべてのイベント処理のプロミスを格納する配列。
-    let events_processed = [];
-
-    // イベントオブジェクトを順次処理。
-    req.body.events.forEach((event) => {
-        // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
-        if (event.type == "message" && event.message.type == "text"){
-            // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
-            if (event.message.text == "こんにちは"){
-                // replyMessage()で返信し、そのプロミスをevents_processedに追加。
-                events_processed.push(client.replyMessage(event.replyToken, {
-                    type: "text",
-                    text: "これはこれは"
-                }));
-            }
-        }
-    });
-
-    // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
-    Promise.all(events_processed).then(
-        (response) => {
-            console.log(`${response.length} event(s) processed.`);
-        }
-    );
+    Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result));
 });
+
+function handleEvent(event) {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+      return Promise.resolve(null);
+    }
+  
+    return client.replyMessage(event.replyToken, {
+      type: 'text',
+      text: event.message.text
+    });
+  }
+  
